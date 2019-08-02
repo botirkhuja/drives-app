94e82418e14a718cb73acd514e806ffac3f57a35
botirkhuja <botir90@gmail.com>
2019-08-02 16:40:27 -0500

    getting user specific data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +8,20 @@ import { AngularFirestore } from '@angular/fire/firestore';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  user
 
-  constructor(db: AngularFirestore) {
-    db.collection('drivers').get().subscribe( response => {
+  constructor(private afs: AngularFirestore, private afAuth: AngularFireAuth ) {
+    const user = this.afAuth.user.subscribe(user => {
+      if (user) {
+        this.user = user;
+        this.getInformation();
+      }
+    })
+
+  }
+
+  getInformation(){
+    this.afs.collection('drivers', ref => ref.where('cell', '==', this.user.phoneNumber)).get().subscribe( response => {
       response.docs.forEach( doc => {
         console.log(doc.data())
 
